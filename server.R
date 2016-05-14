040050030a2c069393d7b7114d8d0309ef090e6c
PrzemekX <przemek7574@gmail.com>
2016-05-14 19:28:14 +0200

    -Dodanie panelu do mapy
    -Dodanie oddzielnego podglądu mapy oraz plotów

--- server.R
+++ server.R
@@ -8,27 +8,38 @@
 # install.packages("shiny")
 # install.packages("leaflet")
 # install.packages("jsonlite")
+# install.packages("ggplot2")
+# install.packages("xlsx")
 
+library(xlsx)
 library(shiny)
 library(leaflet)
 library(jsonlite)
+library(ggplot2)
 
 shinyServer(function(input, output) {
   
   output$mymap <- renderLeaflet({
-    
+
+    ###Inne - OPEN###
+    ###Inne - CLOSE###
+
     ####Radio buttons - Open#####
-    Input1 <- input$Map1_1
-    if(Input1[1] == 1) {
-      x <- 8.223
-      y <- 46.695
-      z <- 6
-    }
-    if (Input1[1] == 2) {
-      x <- 19.775
-      y <- 51.713
-      z <- 5
-    }
+    x <- 8.223
+    y <- 46.695
+    z <- 6
+
+    # Input1 <- input$Map1_1
+    # if(Input1[1] == 1) {
+    #   x <- 8.223
+    #   y <- 46.695
+    #   z <- 6
+    # }
+    # if (Input1[1] == 2) {
+    #   x <- 19.775
+    #   y <- 51.713
+    #   z <- 5
+    # }
     #####Radio buttons - Close#####
 
     #####Icon - Open#####
@@ -168,45 +179,54 @@ shinyServer(function(input, output) {
       iconAnchorX = 12, iconAnchorY = 25
     )
     #####Icon - Close#####
-    
+
+    ChooseMaps <- input$choosemap
     #####GeoJson - Open#####
     geojson <- readLines("SDane2.txt", warn = FALSE) %>%
       paste(collapse = "\n") %>%
       fromJSON(simplifyVector = FALSE)
 
-    # Default styles for all features
-    geojson$style = list(
-      weight = 1,
-      color = "#555555",
-      opacity = 1,
-      fillOpacity = 0.8
-    )
-    
-    # Gather population estimate from all cantons
-    population <- sapply(geojson$features, function(feat) {
-      max(1, feat$MyData$population)
-    })
-    
-    # Color by population using quantiles
-    pal <- colorQuantile("Reds", n = 26, population)  #"Greens"
-    # Add a properties$style list to each feature
-    geojson$features <- lapply(geojson$features, function(feat) {
-      feat$properties$style <- list(
-        fillColor = pal(
-          max(1, feat$MyData$population)
-        )
+    # # Default styles for all features
+    if(ChooseMaps == "normalmap") {
+      geojson$style = list(
+        weight = 2,
+        color = "#000000",
+        # opacity = 0,
+        fillOpacity = 0.5
       )
-      feat
-    })
+    } else {
+      geojson$style = list(
+        weight = 1,
+        color = "#555555",
+        opacity = 1,
+        fillOpacity = 0.8
+      )
+      # Gather population estimate from all cantons
+      population <- sapply(geojson$features, function(feat) {
+        max(1, feat$MyData$population)
+      })
+
+      # Color by population using quantiles
+      pal <- colorQuantile("Reds", n = 26, population)  #"Greens"
+      # Add a properties$style list to each feature
+      geojson$features <- lapply(geojson$features, function(feat) {
+        feat$properties$style <- list(
+          fillColor = pal(
+            max(1, feat$MyData$population)
+          )
+        )
+        feat
+      })
+    }
     #####GeoJson - Close#####
-    
+
     m <- leaflet() %>%
       addGeoJSON(geojson) %>%
       # addTiles() %>% #normala mapa
-      addProviderTiles("Stamen.Toner") %>% #mapa toner
+      # addProviderTiles("Stamen.Toner") %>% #mapa toner
       # addRectangles(lng1 = 8.223, lat1 = 46.695, lng2 = 9, lat2 = 48) %>%
       # addMarkers(lng = 8.223, lat = 46.695, popup="Switzerland", icon = switzerland) %>%
-      
+
       ###Add layers as overlayGroup###
       addMarkers(lng = 8.223, lat = 46.695, popup="Switzerland", group="Countries Flag", icon = switzerland) %>%
       addMarkers(lng = 8.6778, lat = 46.7606, popup = "Uri", group="Cantons", icon = uri) %>%
@@ -236,24 +256,43 @@ shinyServer(function(input, output) {
       addMarkers(lng = 6.1430, lat = 46.2048, popup = "Geneva", group="Cantons", icon = geneva) %>%
       addMarkers(lng = 7.1996, lat = 47.3277, popup = "Jura", group="Cantons", icon = jura) %>%
       ###Add layers as overlayGroup - Close###
-      
       ###Panel###
       addLayersControl(
         overlayGroups = c("Countries Flag", "Cantons"),
         options = layersControlOptions(collapsed = FALSE)
-      ) %>%
-      ###Panel - close###
+      ) #%>%
       
-      setView(lng = x, lat = y, zoom = z) #%>%
+    ChooseTiles <- input$choosetile
+    if(ChooseTiles == "normaltile") {
+      m %>% clearTiles() %>% addTiles()
+    } else if(ChooseTiles == "tonertile") {
+      m %>% clearTiles() %>% addProviderTiles("Stamen.Toner")
+    } else if(ChooseTiles == "positrontile") {
+      m %>% clearTiles() %>% addProviderTiles("CartoDB.Positron")
+    }
+  })
     
-      # addLayersControl(
-      #   overlayGroups = c("PCT", "Southern Terminus", "Northern Terminus", "States"),
-      #   options = layersControlOptions(collapsed = FALSE)
-      # ) %>%
+  output$Plot1 <- renderPlot({
     
+    dataFromExcel <- read.xlsx(file="Dane_Szwajcaria_Wiek3.xlsx", sheetIndex=1,header=TRUE)
+    df <- dataFromExcel
     
-    m  # Print the map
+    # df$Date <- as.Date(as.character(df$Date), format="%Y-%m-%d")
+    x <- df$Time # first column with Date
+    y <- df[ , 2:length(df)] # (all columns from df without the first one, the first column was x = Date)
+    plotGgplot <- ggplot() +
+      geom_line(data = df, aes(x = x, y = y$PopTotal, color=" Total population "), linetype = 2, size = 1.6) +
+      geom_line(data = df, aes(x = x, y = y$PopMale, color=" Male "), linetype = 2, size = 1.6) +
+      geom_line(data = df, aes(x = x, y = y$PopFemale, color=" Female "), linetype = 2, size = 1.6) +
+      ylab('Population') +
+      xlab('Year') +
+      # scale_x_continuous(labels = comma) +
+      scale_y_continuous ( breaks = seq(from=0,to=15000000,by=1000000)) +
+      ggtitle("Switzerland population in 1950-2099") +
+      theme(plot.title=element_text(size=8, face="bold",
+                                    hjust = 0.5),
+            axis.title=element_text(size=8))
+    plotGgplot
     
   })
-
 })
--- ui.R
+++ ui.R
@@ -7,11 +7,50 @@
 library(shiny)
 library(leaflet)
 
-shinyUI(fluidPage(
-  leafletOutput("mymap"),
+#####Choices for drop-downs#####OPEN#####
+vars <- c(
+  "Normal Map" = "normalmap",
+  "Population Map" = "populationmap"#,
+  #"College education" = "college",
+)
+vars2 <- c(
+  "Normal Tile" = "normaltile",
+  "Toner Tile" = "tonertile",
+  "Positron Tile" = "positrontile"
+)
+#####Choices for drop-downs#####CLOSE#####
+
+shinyUI(navbarPage("Dane Telecom", #id="nav",
+  #####TabPanel####Interactive Map#####Open#####
+  tabPanel("Interactive map",
+    # div(class="outer",
+    leafletOutput("mymap", width = "100%", height = 600),
   
-  radioButtons("Map1_1", label = h3("Wyb kraju"),
-               choices = list("Szwajcaria" = 1, "Polska" = 2),
-               selected = 1)
+    ###Map Panel - OPEN###
+    absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                  draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                  width = 350, height = "auto",
+                
+    h2("Panel mapy"),
+    
+    selectInput("choosemap", "Wybór mapy", vars),
+    selectInput("choosetile", "Rodzaj mapy", vars2)
+    
+    # plotOutput("Plot1")
+    ),
+    ###Map Panel - CLOSE###
+
+    tags$head(
+      # Include our custom CSS
+      includeCSS("styles.css")
+    )
+  ),
+  #####TabPanel####Interactive Map#####Close#####
+  
+  #####TabPanel####Plots#####Open#####
+  tabPanel("Plots",
+    plotOutput("Plot1")
+  )
+  #####TabPanel####Plots#####Close#####
                  
 ))
